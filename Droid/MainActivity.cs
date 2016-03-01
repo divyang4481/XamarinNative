788628c3187c68634df526c8398d0963912fd6ea
Kerry W. Lothrop <kerry@lothrop.de>
2016-03-01 17:40:20 +0100

    Initial version.

---
+++ Droid/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using XamarinShared;
+
+namespace Droid
+{
+    [Activity(Label = "Droid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            var button = FindViewById<Button>(Resource.Id.MyButton);
+
+            button.Click += (sender, args) =>
+            {
+                var left = int.Parse(FindViewById<EditText>(Resource.Id.left).Text);
+                var right = int.Parse(FindViewById<EditText>(Resource.Id.right).Text);
+                FindViewById<TextView>(Resource.Id.result).Text = CLib.Add(left, right)
+                    .ToString(CultureInfo.InvariantCulture);
+            };
+        }
+    }
+}
+
---
+++ XamarinShared/CLib.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace XamarinShared
+{
+    public static class CLib
+    {
+#if __IOS__
+        [DllImport("__Internal", EntryPoint = "clib_add")]
+#else
+        [DllImport("libCLib", EntryPoint = "clib_add")]
+#endif
+        public static extern int Add(int left, int right);
+    }
+}
